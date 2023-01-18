5346a4ebe7117c2b51515473c42f88498d4d87bc
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-01-18 21:08:45 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int max = 0;
+int min = 0;
+
+Console.WriteLine("Сейчас узнаем max и min значения из двух чисел! Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min); //Могу написать инче, чтобы выдавал только max значение
